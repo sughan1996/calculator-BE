1af65e5b9f80293a7cdc15712fab891ed4a66122
Sughan Richardson Samson Babuji <sughanrichardson1996@gmail.com>
2023-11-21 18:33:41 -0500

    Update local_script.py

--- UnitTest/local_script.py
+++ UnitTest/local_script.py
@@ -1,4 +1,5 @@
 from CalculatorApp.app import *
+from CalculatorQuantApp.app import *
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -7,6 +8,9 @@ logger = logging.getLogger()
 
 if __name__ == '__main__':
     event = {'a': 5, 'b': 7}
+    logger.info(lambda_handler_add(event))
+    logger.info(lambda_handler_subtract(event))
+    logger.info(lambda_handler_multiply(event))
     logger.info(lambda_handler_add(event))
     logger.info(lambda_handler_subtract(event))
     logger.info(lambda_handler_multiply(event))
