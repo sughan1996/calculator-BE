31d225a0f5dfb21dc9a5811f14ad123fa5ef8f39
Sughan Richardson Samson Babuji <sughanrichardson1996@gmail.com>
2023-11-21 18:43:42 -0500

    Update local_script.py

--- UnitTest/local_script.py
+++ UnitTest/local_script.py
@@ -8,9 +8,9 @@ logger = logging.getLogger()
 
 if __name__ == '__main__':
     event = {'a': 5, 'b': 7}
-    logger.info(lambda_handler_add(event))
-    logger.info(lambda_handler_subtract(event))
-    logger.info(lambda_handler_multiply(event))
+    logger.info(lambda_handler_quant_add(event))
+    logger.info(lambda_handler_quant_multiply(event))
+    logger.info(lambda_handler_quant_subtract(event))
     logger.info(lambda_handler_add(event))
     logger.info(lambda_handler_subtract(event))
     logger.info(lambda_handler_multiply(event))
